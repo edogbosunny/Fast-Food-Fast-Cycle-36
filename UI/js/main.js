91423d59efeeee0f06b4dd88118b8e7162256820
edogbosunny <edogbosunday@gmail.com>
2018-09-25 17:35:52 +0100

    :sparkles: UI-Tuneup-ch-Ui-#160745605

---
+++ UI/js/main.js
@@ -0,0 +1,22 @@
+const plus = document.getElementById('plus').value;
+const minus = document.getElementById('subtract').value;
+let defval = (document.getElementById('quant').innerHTML.value);
+const inp = document.getElementById('quant').value;
+defval = 0;
+console.log(defval);
+
+console.log(inp);
+function plsbtn() {
+  newinp = defval + 1;
+  document.getElementById('quant').innerHTML = newinp;
+  console.log(newinp);
+}
+
+function subbtn() {
+  //  document.getElementById('subtract').disabled = true;
+  newinp = defval - 1;
+  document.getElementById('quant').innerHTML = newinp;
+  console.log(newinp);
+
+  // let sign = Math.sign(newinp)
+}
