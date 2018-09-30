5bc249c288a61a4a191abc935c04cb9433dc725d
edogbosunny <edogbosunday@gmail.com>
2018-09-30 20:55:43 +0100

    refractor chai test

--- server/models/orderDB.js
+++ server/models/orderDB.js
@@ -20,6 +20,27 @@ const userOrder = [
       },
     ],
   },
+  {
+    orderId: 2,
+    status: 'pending',
+    createdDate: 'Wed Aug 22 2018 13:07:44 GMT+0100 (W. Central Africa Standard Time)',
+    Amount: '800',
+
+    items: [
+      {
+        foodId: '4',
+        meal: 'Shaki',
+        price: 400,
+        date: 'Wed Aug 22 2018 13:07:44 GMT+0100 (W. Central Africa Standard Time)',
+      },
+      {
+        foodId: '5',
+        meal: 'Shaki',
+        price: 400,
+        date: 'Wed Aug 22 2018 13:07:44 GMT+0100 (W. Central Africa Standard Time)',
+      },
+    ],
+  },
 ];
 
 export default userOrder;
