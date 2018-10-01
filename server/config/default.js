493cbadad617fa7d8ec2e08da2a6332f7dffee4c
edogbosunny <edogbosunday@gmail.com>
2018-10-01 13:18:36 +0100

    ft(Refractor token Secret)[Delivers #160889223]

--- server/config/default.js
+++ server/config/default.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const defaultConfig = {
-  tokenSecret: 'dhkmsgseieYhb79Nj' || process.env.TOKEN_SECRET,
+  tokenSecret: 'dhkmsgseieYhb79fh' || process.env.TOKEN_SECRET,
 };
 
 export default defaultConfig;
