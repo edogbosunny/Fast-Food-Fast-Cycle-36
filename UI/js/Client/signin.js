c844c14f652959a60cf3bccbe54ce70d09c16e1c
edogbosunny <edogbosunday@gmail.com>
2018-10-15 16:13:03 +0100

    ch(consumeSigninApi)[Finishes #161098556]

--- UI/js/Client/signin.js
+++ UI/js/Client/signin.js
@@ -1,17 +1,12 @@
-const baseUrl = 'https://fast-food-fast-app.herokuapp.com/api/v1/auth/login';
-// const signUpForm = document.querySelector('.signup');
+const baseUrl = 'http://localhost:4000/api/v1/auth/login';
 const submitBtn = document.querySelector('#submitbtn');
 const loader = (document.getElementById('loader').style.display = 'none');
-// console.log('----->', loader);
-
 let uReg;
-
 const signinUser = e => {
   e.preventDefault();
   document.getElementById('loader').style.display = 'block';
   const email = document.getElementById('email').value;
-  const pass = document.getElementById('pass').value;
-//   console.log(pwd)
+  let password = document.getElementById('pass').value;
   const emailErrMsg = document.getElementById('emailErrMsg');
   const passErrMsg = document.getElementById('pwdErrMsg');
   const signinErrMsg = document.getElementById('signinErrMsg');
@@ -20,11 +15,9 @@ const signinUser = e => {
   fetch(baseUrl, {
     method: 'POST',
     body: JSON.stringify({
-      pass,
         email,
-      
+        password
     }),
-    
     headers: {
       'Content-Type': 'application/json'
     }
@@ -34,7 +27,6 @@ const signinUser = e => {
     })
     .then(data => {
         setTimeout(() => {
-            console.log(data);
           if (data.status === true) {
             localStorage.setItem('x-auth-token', data.data.token);
             // window.location.replace('/index.html');
@@ -44,15 +36,17 @@ const signinUser = e => {
 
       document.getElementById('loader').style.display = 'none';
       if (data.data.error === undefined) {
-        console.log(data);
-        console.log('=====>', data.data.message);
+    if(data.data.message.length > 0) {
+        emailErrMsg.innerHTML = '';
+        passErrMsg.innerHTML = '';
+
+    }
         uSignin = signinErrMsg.innerHTML = data.data.message;
         return data;
       }
 
       let mail = (emailErrMsg.innerHTML = data.data.error.email);
       let pass = (passErrMsg.innerHTML = data.data.error.password);
-      // console.log('=====> ', uReg);
       if (mail === undefined) {
         emailErrMsg.innerHTML = '';
       }
